35c7c32876372e061d349e61bd6c6b242cbbf177
Víctor Alba <33451213+victoralba@users.noreply.github.com>
2019-11-21 20:39:54 +0100

    Initial commit

---
+++ todo.js
@@ -0,0 +1,74 @@
+const app = new Vue({
+    el: '#app',
+    data : {
+        titulo: 'TODO-TODAY',
+        tareas: [],
+        nuevaTarea: '',
+        // Esto es igual pero para la lista de tareas completas
+        tareasCompletadas: [],
+        tareaCompleta: '',
+    },
+    methods:{
+        agregarTarea: function(){
+            this.tareas.push({
+                nombre: this.nuevaTarea,
+                estado: false,
+            });
+            this.nuevaTarea = '';
+            localStorage.setItem('lista_tareas', JSON.stringify(this.tareas));
+            localStorage.setItem('lista_tareas_completa', JSON.stringify(this.tareasCompletadas));
+
+        },
+        //Este va a ser el nuevo completar, cuando lo sepa hacer
+        editarTarea: function(index){
+            this.tareas[index].estado = true;
+            localStorage.setItem('lista_tareas', JSON.stringify(this.tareas));
+            localStorage.setItem('lista_tareas_completa', JSON.stringify(this.tareasCompletadas));
+
+        },
+        eliminar: function(index){
+            this.tareas.splice(index, 1);
+            localStorage.setItem('lista_tareas', JSON.stringify(this.tareas));
+            localStorage.setItem('lista_tareas_completa', JSON.stringify(this.tareasCompletadas));
+
+        },
+
+        completarTarea: function(index){
+            //Elimino la tarea de las tareas, y la creo en tareas completadas
+            nombre_temp = this.tareas[index].nombre;
+            this.tareas.splice(index, 1);
+            this.tareasCompletadas.push({
+                nombre: nombre_temp,
+                estado: true,
+            });
+            localStorage.setItem('lista_tareas', JSON.stringify(this.tareas));
+            localStorage.setItem('lista_tareas_completa', JSON.stringify(this.tareasCompletadas));
+
+        },
+        eliminarCompleta: function(index){
+            this.tareasCompletadas.splice(index, 1);
+            localStorage.setItem('lista_tareas', JSON.stringify(this.tareas));
+            localStorage.setItem('lista_tareas_completa', JSON.stringify(this.tareasCompletadas));
+
+        }
+    },
+    created: function(){
+        let datos_tareas = JSON.parse(localStorage.getItem('lista_tareas'))
+        if (datos_tareas === null){
+            this.tareas = [];
+        }
+        else{
+            this.tareas = datos_tareas;
+        }
+
+        let datos_tareas_complet = JSON.parse(localStorage.getItem('lista_tareas_completa'))
+        if (datos_tareas_complet === null){
+            this.tareasCompletadas = [];
+        }
+        else{
+            this.tareasCompletadas = datos_tareas_complet;
+        }
+    }
+    
+
+});
